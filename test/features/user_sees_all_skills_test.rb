eb4ded52542c9d4cfbefe3c12b8776b88a96fa5d
Chad Ellison <chad.ellison0123@gmail.com>
2016-02-06 18:11:39 -0700

    another feature test

--- test/features/user_sees_all_skills_test.rb
+++ test/features/user_sees_all_skills_test.rb
@@ -1,11 +1,19 @@
 require_relative "../test_helper"
 
 class UserSeesAllSkillsTest < FeatureTest
+  include TestHelpers
   def test_user_sees_all_skills
+    data = {
+      title: "skill",
+      description: "description"
+    }
+
+    skill = skill_inventory.create(data)
+
     visit "/skills"
 
     within "h3" do
-      assert page.has_content? ""
+      assert page.has_content? "skill"
     end
   end
 end
